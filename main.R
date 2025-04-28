8efeea07270b9ca85550458b6899ed0fb395ee35
b8niiii <120273016+b8niiii@users.noreply.github.com>
2025-04-28 19:16:27 +0200

    Deepen the correlation analysis

--- main.R
+++ main.R
@@ -1,9 +1,24 @@
+''' https://www.kaggle.com/datasets/rohan0301/unsupervised-learning-on-country-data
+
+Problem Statement:
+  HELP International have been able to raise around $ 10 million. 
+Now the CEO of the NGO needs to decide how to use this money strategically 
+and effectively. So, CEO has to make decision to choose the countries that are 
+in the direst need of aid. Hence, your Job as a Data scientist is to categorise
+the countries using some socio-economic and health factors that determine the 
+overall development of the country. Then you need to suggest the countries 
+which the CEO needs to focus on the most. '''
+
+
 library(corrplot)
 library(car)
 library(Hmisc) # for redundancy analysis
 library(ggplot2)
 library(gridExtra)
 library(dplyr)
+library(plotly)
+library(glmnet) # for ridge regression
+library(reshape2) # for reshaping the data easily
 
 data <- read.csv("data/Country-data.csv", sep = ",", header = TRUE, na.strings = c("", "NA", "NULL")) # Read the CSV file
 dict <- read.csv("data/data-dictionary.csv", sep = ",", header = TRUE, na.strings = c("", "NA", "NULL")) # Read the CSV file
@@ -202,6 +217,9 @@ print(redun_result)
 redun_result <- redun(~ ., data = data_log, nk = 0) 
 print(redun_result)
 
+# LET'S TRY TO GET A LIL BIT DEEPER INTO THE CORRELATIONS AMONG INCOME, GDPP AND CHILD MORT
+# WE SPOTTED THOSE PROBLEMATIC VARIABLES DOING THE CORRELATION MATRIX AND ANALYZING WITH
+# THE VIF AND THE REDUNDANCY ANALYSIS.
 
 # standardize data
 
@@ -216,19 +234,138 @@ print(redun_result_scaled)
 
 # we got the same results as before 
 
+# Simple scatter plot
+ggplot(data_scaled_log, aes(x = gdpp, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  labs(title = "Income vs GDP per capita", x = "GDP per capita", y = "Income") +
+  theme_minimal()
+
+ggplot(data_scaled_log, aes(x = gdpp, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  geom_smooth(method = "lm", color = 'red') +
+  labs(title = "Log-Log Relationship", x = "log(GDP per capita)", y = "log(Income)") +
+  theme_minimal()
+# as we can see this log-log relation is very close to being perfectly linear:
+# so the original (unlogged) relationship between income and gdpp is a power law
+# income ∝( gdpp)^β where β  is the slope of of the log-log line
+# Now report the slope of the regression line (Beta)
+summary(lm(income ~ gdpp, data = data_scaled_log))
+
+# the coefficient is highly significantly equal to 9.720e-01 and the 
+# intercept is 9.044e-16, almost 0 and not significant
+
+# If log(Income) = α + β * log(GDPP), then
+
+# Income = exp(α) × GDPP^β
+
+# Income≈(GDP per capita) ^ 0.972
+
+
+# Correct intercept and beta from your model
+# Define alpha and beta FIRST
+alpha <- 9.044e-16  # your intercept (very small)
+beta <- 0.972       # your slope
+
+# Then plot
+ggplot(data, aes(x = gdpp, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  stat_function(fun = function(x) exp(alpha) * x^beta, color = 'red', size = 1.2) +
+  labs(
+    title = "Income vs GDP per capita (Original Scale with Correct Power-Law Fit)",
+    x = "GDP per capita",
+    y = "Income"
+  ) +
+  theme_minimal()
+
+# not satisfied with the power law fit, let's try to fit a polynomial regression
+# Fit quadratic model
+poly2_model <- lm(income ~ poly(gdpp, 2, raw = TRUE), data = data)
+
+# Plot
+ggplot(data, aes(x = gdpp, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2, raw = TRUE), color = 'red', size = 1.2) +
+  labs(
+    title = "Quadratic Fit: Income vs GDP per capita",
+    x = "GDP per capita",
+    y = "Income"
+  ) +
+  theme_minimal()
+
+# now to officially say that these two are not linearly correlated, I will run a linear model
+# and compare the adjusted R^2:
+linear_model <- lm(income ~ gdpp, data = data)
+summary(linear_model)
+summary(poly2_model)
+
+# matter of facts, the adj R^2 of the polynomial model is 0.8194 and is better than the linear 
+# one of 0.8008 and the RSE is lower on the polynomial (8193 compared to 8603)
+
+# same with child_mort
+
+
+# Simple scatter plot
+ggplot(data_scaled_log, aes(x = child_mort, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  labs(title = "Income vs Child Mortality", x = "Child Mortality", y = "Income") +
+  theme_minimal()
+
+ggplot(data_scaled_log, aes(x = child_mort, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  geom_smooth(method = "lm", color = 'red') +
+  labs(title = "Log-Log Relationship", x = "log(Child Mortality)", y = "log(Income)") +
+  theme_minimal()
+
+# Report the slope of the regression line
+summary(lm(income ~ child_mort, data = data_scaled_log))
+
+# Define alpha and beta
+alpha <- coef(lm(income ~ child_mort, data = data_scaled_log))[1]
+beta <- coef(lm(income ~ child_mort, data = data_scaled_log))[2]
+
+# Plot fitted power-law on original data
+ggplot(data, aes(x = child_mort, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  stat_function(fun = function(x) exp(alpha) * x^beta, color = 'red', size = 1.2) +
+  labs(
+    title = "Income vs Child Mortality (Original Scale with Power-Law Fit)",
+    x = "Child Mortality",
+    y = "Income"
+  ) +
+  theme_minimal()
+
+# Fit quadratic model
+poly2_model <- lm(income ~ poly(child_mort, 2, raw = TRUE), data = data)
+
+# Plot quadratic fit
+ggplot(data, aes(x = child_mort, y = income)) +
+  geom_point(color = 'blue', alpha = 0.6) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2, raw = TRUE), color = 'red', size = 1.2) +
+  labs(
+    title = "Quadratic Fit: Income vs Child Mortality",
+    x = "Child Mortality",
+    y = "Income"
+  ) +
+  theme_minimal()
+
+# Compare linear vs quadratic model
+linear_model <- lm(income ~ child_mort, data = data)
+summary(linear_model)
+summary(poly2_model)
+# also here the adj r^2 of the polynomial model is better than the linear one 
+# 0.380 vs 0.270
+
+
 # Let's do the PCA on the scaled log data now:
 
-pca_log <- prcomp(data_scaled_log)
+pca_result <- prcomp(data_scaled_log)
 summary(pca_result)
-# Plot PCA results
-par(mfrow = c(1, 2)) # Set up a 1x2 grid for plots
-plot(pca_result, main = "PCA Scree Plot") # Scree plot
-biplot(pca_result, main = "PCA Biplot") # Biplot
+
 
 # PCA from old project
 
-pca_log_var <- pca_log$sdev^2 # variance of each principal component
-pca_var_per <- round(pca_log_var/sum(pca_log_var)*100, 2) # percentage of variance explained by each component
+pca_result_var <- pca_result$sdev^2 # variance of each principal component
+pca_var_per <- round(pca_result_var/sum(pca_result_var)*100, 2) # percentage of variance explained by each component
 
 par(mfrow = c(1,2), mar = c(5,5,4,2)) # mar = bottom, left, top, right
 scree_plot <-barplot(pca_var_per, 
@@ -256,28 +393,45 @@ optimal_pcs_num <- 4
 points(optimal_pcs_num, residvar[optimal_pcs_num], col = '#D22B2B', pch = 19, cex = 2) # highlight the optimal number of components
 text(optimal_pcs_num, residvar[optimal_pcs_num], , labels = paste(optimal_pcs_num, 'PCs ~ 90.81% Variance Explained'), pos = 3, col = '#D22B2B')
 
+
+# Set plotting layout to 1x1 (one plot per window)
 par(mfrow = c(1,1)) 
-rownames(pca_log$x) <-1:nrow(pca_log$x) # Set row names to be the row numbers
-pca_data <-as.data.frame(pca_log$x)
-pca.data.summary <- data.frame(sample=rownames(pca_log$x),
-                               X=pca_log$x[,1],
-                               Y=pca_log$x[,2],
-                               Z=pca_log$x[,3],
-                               row.names=NULL)
 
-pca.loadings <- data.frame(variable=names(data_log),
-                           X=pca_log$rotation[,1],
-                           Y=pca_log$rotation[,2],
-                           Z=pca_log$rotation[,3],
-                           row.names=NULL)
+# Rename rows of PCA scores with simple numbers
+rownames(pca_result$x) <- 1:nrow(pca_result$x) 
 
-#copy-pasted from the project with Ali:
+# Convert PCA scores to a data frame
+pca_data <- as.data.frame(pca_result$x)
+
+# Create a data frame summarizing sample coordinates on the first 4 principal components
+pca.data.summary <- data.frame(  # creates a dataframe where each row is a sample from my data
+  sample = rownames(pca_result$x), # and as columns there are its coordinates in Principal Component space
+  X = pca_result$x[,1],
+  Y = pca_result$x[,2],
+  Z = pca_result$x[,3],
+  W = pca_result$x[,4],
+  row.names = NULL # row names are set to NULL to avoid confusion
+)
+
+# Create a data frame summarizing feature loadings on the first 3 principal components
+pca.loadings <- data.frame(
+  variable = names(data_log), # loadings tell you how much each original feature
+  X = pca_result$rotation[,1],   # contributed to each PC. 
+  Y = pca_result$rotation[,2],   # Technically loadings are the correlation from each original 
+  Z = pca_result$rotation[,3],   # variable and the principal components
+  W = pca_result$rotation[,4],
+  row.names = NULL
+)
+
+
 # Normalize sample scores to fit within [-1,1] for better visualization
 pca.data.summary$X <- pca.data.summary$X / max(abs(pca.data.summary$X))
 pca.data.summary$Y <- pca.data.summary$Y / max(abs(pca.data.summary$Y))
 pca.data.summary$Z <- pca.data.summary$Z / max(abs(pca.data.summary$Z))
+pca.data.summary$W <- pca.data.summary$W / max(abs(pca.data.summary$W))
 
-circle <- data.frame(
+
+circle <- data.frame( # create a circle for the biplot
   x = cos(seq(0, 2 * pi, length.out = 1000)),
   y = sin(seq(0, 2 * pi, length.out = 1000))
 )
@@ -327,16 +481,61 @@ biplot_pc1v3 <- ggplot() +
   theme_bw() +
   ggtitle('PCA Biplot (PC1 vs PC3)')
 
-grid.arrange(biplot_pc1v2, biplot_pc2v3, biplot_pc1v3, ncol = 3)
+# PC1 vs PC4
+biplot_pc1v4 <- ggplot() +
+  geom_point(data = pca.data.summary, aes(x = X, y = W), color = 'lightblue', alpha = 0.6) +  
+  geom_segment(data = pca.loadings, aes(x = 0, y = 0, xend = X, yend = W), 
+               arrow = arrow(length = unit(0.2, 'cm')), color = '#D22B2B') +
+  geom_text(data = pca.loadings, aes(x = X, y = W, label = variable), 
+            size = 3.5, color = '#D22B2B', vjust = 1.5) +  
+  geom_path(data = circle, aes(x = x, y = y), color = 'lightblue', linetype = 'dashed') +
+  xlab(paste('PC1 - ', pca_var_per[1], '%', sep = '')) +
+  ylab(paste('PC4 - ', pca_var_per[4], '%', sep = '')) +
+  xlim(-1, 1) + ylim(-1, 1) +  
+  coord_fixed() +
+  theme_bw() +
+  ggtitle('PCA Biplot (PC1 vs PC4)')
+
+# PC2 vs PC4
+biplot_pc2v4 <- ggplot() +
+  geom_point(data = pca.data.summary, aes(x = Y, y = W), color = 'lightblue', alpha = 0.6) +  
+  geom_segment(data = pca.loadings, aes(x = 0, y = 0, xend = Y, yend = W), 
+               arrow = arrow(length = unit(0.2, 'cm')), color = '#D22B2B') +
+  geom_text(data = pca.loadings, aes(x = Y, y = W, label = variable), 
+            size = 3.5, color = '#D22B2B', vjust = 1.5) +  
+  geom_path(data = circle, aes(x = x, y = y), color = 'lightblue', linetype = 'dashed') +
+  xlab(paste('PC2 - ', pca_var_per[2], '%', sep = '')) +
+  ylab(paste('PC4 - ', pca_var_per[4], '%', sep = '')) +
+  xlim(-1, 1) + ylim(-1, 1) +  
+  coord_fixed() +
+  theme_bw() +
+  ggtitle('PCA Biplot (PC2 vs PC4)')
+
+# PC3 vs PC4
+biplot_pc3v4 <- ggplot() +
+  geom_point(data = pca.data.summary, aes(x = Z, y = W), color = 'lightblue', alpha = 0.6) +  
+  geom_segment(data = pca.loadings, aes(x = 0, y = 0, xend = Z, yend = W), 
+               arrow = arrow(length = unit(0.2, 'cm')), color = '#D22B2B') +
+  geom_text(data = pca.loadings, aes(x = Z, y = W, label = variable), 
+            size = 3.5, color = '#D22B2B', vjust = 1.5) +  
+  geom_path(data = circle, aes(x = x, y = y), color = 'lightblue', linetype = 'dashed') +
+  xlab(paste('PC3 - ', pca_var_per[3], '%', sep = '')) +
+  ylab(paste('PC4 - ', pca_var_per[4], '%', sep = '')) +
+  xlim(-1, 1) + ylim(-1, 1) +  
+  coord_fixed() +
+  theme_bw() +
+  ggtitle('PCA Biplot (PC3 vs PC4)')
+
+grid.arrange(biplot_pc1v2, biplot_pc2v3, biplot_pc1v3,biplot_pc1v4,biplot_pc2v4,biplot_pc3v4, ncol = 3)
 
 # PCA 3D plot (PC 1,2,3)
-pca_log_scaled <- pca_log$x / max(abs(pca_log$x))
-pca.loadings.scaled <- pca_log$rotation / max(abs(pca_log$rotation))
+pca_result_scaled <- pca_result$x / max(abs(pca_result$x))
+pca.loadings.scaled <- pca_result$rotation / max(abs(pca_result$rotation))
 
-pca_log_plot_threeD <- plot_ly() %>%
-  add_trace(x = pca_log_scaled[,1], 
-            y = pca_log_scaled[,2], 
-            z = pca_log_scaled[,3], 
+pca_result_plot_threeD <- plot_ly() %>%
+  add_trace(x = pca_result_scaled[,1], 
+            y = pca_result_scaled[,2], 
+            z = pca_result_scaled[,3], 
             type = 'scatter3d', mode = 'markers',
             marker = list(color = 'lightblue', size = 3),
             name = 'Samples') %>%
@@ -353,7 +552,7 @@ pca_log_plot_threeD <- plot_ly() %>%
             y = pca.loadings.scaled[,2], 
             z = pca.loadings.scaled[,3], 
             type = 'scatter3d', mode = 'text',
-            text = rownames(pca_log$rotation),
+            text = rownames(pca_result$rotation),
             textposition = 'top center',
             textfont = list(color = '#D22B2B', size = 12),
             name = 'Variables') %>%
@@ -361,7 +560,7 @@ pca_log_plot_threeD <- plot_ly() %>%
          scene = list(xaxis = list(title = paste('PC1 - ', pca_var_per[1], '%')),
                       yaxis = list(title = paste('PC2 - ', pca_var_per[2], '%')),
                       zaxis = list(title = paste('PC3 - ', pca_var_per[3], '%'))))
-
+pca_result_plot_threeD
 
 
 
